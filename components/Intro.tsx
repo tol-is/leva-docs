132ea2c830bc1304b6509dd89b39d04002ad9bd8
Apostolos Christodoulou <a7sc11u@gmail.com>
2022-02-07 00:45:11 +0000

    add intro id

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,9 +4,15 @@ import { H1, Large } from "@components/Text";
 import { Box } from "./UI";
 
 export const Intro = ({ title, description, ...rest }) => {
+  const id = title
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replaceAll("_", "-")
+    .replaceAll("--", "-")
+    .slice(0, 200);
   return (
     <Box as="header" {...rest}>
-      <H1 id="#">{title}</H1>
+      <H1 id={id}>{title}</H1>
       <Large css={{ marginTop: "$3" }}>{description}</Large>
     </Box>
   );
